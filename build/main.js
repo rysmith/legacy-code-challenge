32798fceffdefb396a3ec0810a0d2221038faf51
rysmith <ryansmith05@gmail.com>
2015-08-06 14:36:32 -0700

    add .gitignore, build MyComponent, closes #1

--- build/main.js
+++ build/main.js
@@ -1,15 +1,36 @@
 var MyComponent = React.createClass ({displayName: "MyComponent",
+    getInitialState: function () {
+        return {text: true}
+    },
+    handleButtonClick: function () {
+        this.setState({text: !this.state.text})
+    },
     render: function () {
+        var showText = this.state.text ? 'visible' : 'hidden';
         return (
             React.createElement("div", {className: "myComponent"}, 
-                "hello world!"
+                React.createElement("span", null, this.props.text), React.createElement("br", null), 
+                React.createElement(MyButton, null)
             )
+
+
         )
     }
 });
 
+var MyButton = React.createClass ({displayName: "MyButton",
+   render: function () {
+       return (
+           React.createElement("button", {className: "myButton"}, "Click to Toggle Text")
+       )
+
+   }
+});
+
+var text = 'Here is some text that should show by default and hide on button click.';
+
 React.render(
-    React.createElement(MyComponent, null),
+    React.createElement(MyComponent, {text: text}),
     document.getElementById('content')
 );
 
--- src/main.js
+++ src/main.js
@@ -1,15 +1,36 @@
 var MyComponent = React.createClass ({
+
+    // initial state of showText has been set to true
+    getInitialState: function () {
+        return {showText: true}
+    },
+
+    // once the button is clicked, change the state of showText to the opposite of it's current value
+    handleButtonClick: function () {
+        this.setState({showText: !this.state.showText})
+    },
+
+    // render text and MyButton component
     render: function () {
         return (
-            <div className="myComponent">
-                hello world!
-            </div>
+            <span className="myComponent">{this.props.text}</span><br/>
+            <MyButton></MyButton>
         )
     }
 });
 
+var MyButton = React.createClass ({
+    render: function () {
+        return (
+            <button className="myButton">Click to Toggle Text</button>
+        )
+    }
+});
+
+var text = 'Here is some text that should show by default and hide on button click.';
+
 React.render(
-    <MyComponent/>,
+    <MyComponent text={text}/>,
     document.getElementById('content')
 );
 
